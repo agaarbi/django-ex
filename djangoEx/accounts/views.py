0886394ac4300829f7435d57ac7c811782c11c20
ghani <agaarbi@gmail.com>
2022-09-07 12:11:07 +0500

    login logout

--- djangoEx/accounts/views.py
+++ djangoEx/accounts/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.models import User, auth
 # Create your views here.
 
+def login(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            messages.info(request, 'invalid credentials')
+            return redirect("login")
+
+    else:
+        return render(request, 'login.html')
 
 def register(request):
 
@@ -15,16 +32,24 @@ def register(request):
 
         if password1==password2:
             if User.objects.filter(username=username).exists():
-                print('username taken')
+                messages.info(request, 'username taken')
+                return redirect('register')
             elif User.objects.filter(email=email).exists():
-                print('email taken')
+                messages.info(request,'email taken')
+                return redirect('register')
             else:
                 user = User.objects.create_user(username=username, password=password1, email= email, first_name=first_name, last_name=last_name)
                 user.save()
                 print('user created')
+                return redirect('login')
         else:
-            print('password not matching.')
+            messages.info(request,'password not matching.')
+            return redirect('register')
         return redirect('/')
 
     else:
         return render(request, 'register.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/') 
